3f1d02b5121b2d6c89c85bfb35a2e0cd43858829
Acker404 <mark921202@gmail.com>
2025-08-03 03:47:18 +0800

    Clean untracked binary files

---
+++ code/Qt_yolo_1/detectionworker.h
@@ -0,0 +1,33 @@
+#ifndef DETECTIONWORKER_H
+#define DETECTIONWORKER_H
+
+#include <QObject>
+#include <QThread>
+#include <QImage>
+#include "inference.h"
+#include <opencv2/core/mat.hpp>
+
+class DetectionWorker : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit DetectionWorker(Inference* inference, QObject *parent = nullptr);
+    ~DetectionWorker();
+
+public slots:
+    void processImage(const cv::Mat& image);
+    void processFrame(const cv::Mat& frame);
+    void stop();
+
+signals:
+    void detectionFinished(const cv::Mat& result);
+    void finished();
+
+private:
+    void drawDetection(cv::Mat& frame, const Detection& detection);
+    Inference* pIntf_;
+    bool m_stop = false;
+};
+
+#endif // DETECTIONWORKER_H
